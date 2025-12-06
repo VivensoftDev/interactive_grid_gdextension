54e4bd87c4c51041db1fb929331f3e75d00e92c0
antoine.charruel <charruel.antoine@outlook.com>
2025-12-06 16:48:00 +0100

    chore: clean up code

--- src/custom_cell_data.cpp
+++ src/custom_cell_data.cpp
@@ -1,78 +1,76 @@
+/**************************************************************************/
+/*  custom_cell_data.cpp                                                  */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                     INTERACTIVE GRID GDExtension                       */
+/*         https://github.com/antoinecharruel/interactive_grid            */
+/**************************************************************************/
+/* Copyright (c) 2025 Antoine Charruel.                                   */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
 #include "custom_cell_data.h"
 
 void CustomCellData::set_custom_data_name(godot::String p_name) {
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 	custom_data_name = p_name;
 }
 
 godot::String CustomCellData::get_custom_data_name() const {
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 	return custom_data_name;
 }
 
 void CustomCellData::set_layer_mask(const uint32_t p_layer_mask) {
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 	layer_mask = p_layer_mask;
 }
 
 uint32_t CustomCellData::get_layer_mask() const {
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 	return layer_mask;
 }
 
 void CustomCellData::set_collision_layer(uint32_t p_layer) {
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 	collision_layer = p_layer;
 }
 
 uint32_t CustomCellData::get_collision_layer() const {
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 	return collision_layer;
 }
 
 void CustomCellData::set_color(const godot::Color &p_color) {
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 	color = p_color;
 }
 
 godot::Color CustomCellData::get_color() const {
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
 	return color;
 }
 
-void CustomCellData::set_custom_color_enabled(bool p_enabled){
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-  custom_color_enabled = p_enabled;
+void CustomCellData::set_custom_color_enabled(bool p_enabled) {
+	custom_color_enabled = p_enabled;
 }
 
-bool CustomCellData::get_custom_color_enabled() const{
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-  return custom_color_enabled;
+bool CustomCellData::get_custom_color_enabled() const {
+	return custom_color_enabled;
 }
-
 
 bool CustomCellData::has_layers_in_mask(const uint32_t p_layer_mask) const {
-    return (collision_layer & p_layer_mask) == collision_layer;
+	return (collision_layer & p_layer_mask) == collision_layer;
 }
 
 bool CustomCellData::get_collision_layer_value(int p_layer_number) const {
@@ -82,10 +80,6 @@ bool CustomCellData::get_collision_layer_value(int p_layer_number) const {
 }
 
 void CustomCellData::_bind_methods() {
-	/*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
-  Summary: // TODO
-  M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
-
 	godot::ClassDB::bind_method(godot::D_METHOD("set_custom_data_name", "name"), &CustomCellData::set_custom_data_name);
 	godot::ClassDB::bind_method(godot::D_METHOD("get_custom_data_name"), &CustomCellData::get_custom_data_name);
 
@@ -94,7 +88,7 @@ void CustomCellData::_bind_methods() {
 
 	godot::ClassDB::bind_method(godot::D_METHOD("set_collision_layer", "layer_mask"), &CustomCellData::set_collision_layer);
 	godot::ClassDB::bind_method(godot::D_METHOD("get_collision_layer"), &CustomCellData::get_collision_layer);
-	
+
 	godot::ClassDB::bind_method(godot::D_METHOD("set_custom_color_enabled", "use_custom_color"), &CustomCellData::set_custom_color_enabled);
 	godot::ClassDB::bind_method(godot::D_METHOD("get_custom_color_enabled"), &CustomCellData::get_custom_color_enabled);
 
--- src/custom_cell_data.h
+++ src/custom_cell_data.h
@@ -1,24 +1,36 @@
-/*+===================================================================
-File: custom_cell_data.h
-
-Summary: // TODO
-
-Last Modified: December 2, 2025
-
-This file is part of the InteractiveGrid GDExtension Source Code.
-Repository: https://github.com/antoinecharruel/interactive_grid
-
-Version InteractiveGrid: 1.7.0
-Version: Godot Engine v4.5.stable.steam - https://godotengine.org
-
-Author: Antoine Charruel
-===================================================================+*/
+/**************************************************************************/
+/*  custom_cell_data.h                                                    */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                     INTERACTIVE GRID GDExtension                       */
+/*         https://github.com/antoinecharruel/interactive_grid            */
+/**************************************************************************/
+/* Copyright (c) 2025 Antoine Charruel.                                   */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
 
 #pragma once
 
 #include "common.h"
 
-// Godot engine
 #include <godot_cpp/classes/resource.hpp>
 #include <godot_cpp/core/class_db.hpp>
 
